f883de0a505612e758b2ca2efa6fbafd85af4271
vengolan <vengolan@gmail.com>
2019-05-22 19:58:29 +0530

    Shell script changes

--- parkinglot/parking_lot.sh
+++ parkinglot/parking_lot.sh
@@ -1,5 +1,17 @@
+GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
+
+
+#Validate input file. If no parameter is passed, execute in command mode
+DIR=$1 || '.'
+
+if  [  "$DIR" -a ! -f "$DIR" ];then
+        echo "${DIR} is an Invalid File!! Exiting...."
+        exit -1
+fi  
+
+
 echo "Starting test..."
 echo "mvn clean test -Dtest=com.coderbyte.parkinglot.ParkingLotTests -q"
 mvn clean test -Dtest=com.coderbyte.parkinglot.ParkingLotTests -q
@@ -19,9 +31,15 @@ fi
 echo -e "${YELLOW}***************${NC}"
 echo -e "${YELLOW}BUILD SUCCESS${NC}"
 echo -e "${YELLOW}***************${NC}"
-echo "Executing Application" 
-echo "java -classpath ./target/parkinglot*jar com.coderbyte.parkinglot.Application parkinglot.txt"
-java -classpath ./target/parkinglot*jar com.coderbyte.parkinglot.Application parkinglot.txt
+
+echo "java -classpath ./target/parkinglot*jar com.coderbyte.parkinglot.Application ${DIR}"
+if [ ! $DIR ]
+then
+	echo -e "${GREEN}Executing Application in Command mode. Please enter Commands:${NC}"
+else 
+	echo "Executing Application"
+fi
+java -classpath ./target/parkinglot*jar com.coderbyte.parkinglot.Application ${DIR}
 echo -e "${YELLOW}***************${NC}"
 echo -e "${YELLOW}COMPLETE${NC}"
 echo -e "${YELLOW}***************${NC}"
