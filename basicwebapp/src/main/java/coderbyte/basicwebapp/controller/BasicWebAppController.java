cb2ed99ded4b00c1f432107fdf11d037ade7a4c9
vengolan <vengolan@gmail.com>
2019-05-25 00:29:40 +0530

    error handling and spring connection property changes

--- basicwebapp/src/main/java/coderbyte/basicwebapp/controller/BasicWebAppController.java
+++ basicwebapp/src/main/java/coderbyte/basicwebapp/controller/BasicWebAppController.java
@@ -1,10 +1,11 @@
 package coderbyte.basicwebapp.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import coderbyte.basicwebapp.dao.BasicWebAppDao;
@@ -17,6 +18,9 @@ public class BasicWebAppController {
     BasicWebAppDao dao;
     
     private HttpServletRequest request;
+    
+    @Autowired
+    private HttpServletResponse response;
 
     @Autowired
     public void setRequest(HttpServletRequest request) {
@@ -48,14 +52,14 @@ public class BasicWebAppController {
      * @return - Return bean that contains IP address and the rank. This to be returned as json to client 
      */
 	@GetMapping(path="getrank")
-	public BasicWebAppBean helloWorldPathVariable()  throws Exception{
-		
+	public BasicWebAppBean helloWorldPathVariable() throws Exception{
+
+
 			//get client IP address
 			String ipAddress = getClientIp();
 			
 			//get rank for this IP address from dao. 
 			return dao.getHitNo(ipAddress);
-
 	
 	}
 	
