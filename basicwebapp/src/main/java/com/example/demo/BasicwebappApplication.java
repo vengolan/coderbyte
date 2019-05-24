f5ba451dd6aaa4fb782a1ca57f054380a0187e2d
vengolan <vengolan@gmail.com>
2019-05-24 13:56:59 +0530

    added Procfile for heroku. changes to Application java file

--- basicwebapp/src/main/java/com/example/demo/BasicwebappApplication.java
+++ basicwebapp/src/main/java/com/example/demo/BasicwebappApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"coderbyte.basicwebapp"})
+@ComponentScan(basePackages = {"coderbyte.basicwebapp.*"})
 public class BasicwebappApplication {
 
 	public static void main(String[] args) {
